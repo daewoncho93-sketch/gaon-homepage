645931a44528f073c17257c2e391feedf56b5e1e
daewoncho93-sketch <daewoncho93@gmail.com>
2025-08-20 17:38:26 +0900

    Delete pages directory

--- pages/index.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">Gaon Homepage</h1>
-    </div>
-  )
-}
